d65ba3f83597c05a8c1b661060367ba324855f69
frc5232 <nmc4frc5232@gmail.com>
2026-03-15 13:26:45 -0500

    Changed auto

--- src/main/java/frc/robot/subsystems/Auto.java
+++ src/main/java/frc/robot/subsystems/Auto.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -45,7 +46,7 @@ public class Auto extends SubsystemBase {
     
     // we can tune this some more when we are at arc field
     // this is for path of the auto from start to where we want to be
-    ltvController = new LTVUnicycleController(VecBuilder.fill(1, 1, 2.0), VecBuilder.fill(10.0, 20.0), 0.02, 9);
+    ltvController =new LTVUnicycleController(VecBuilder.fill(0.0625, 0.125, 2.0), VecBuilder.fill(1.0, 2.0), 0.02, 9);
     
   }
 
@@ -190,7 +191,7 @@ public class Auto extends SubsystemBase {
     // if (DriverStation.isAutonomous() && autoHasBeenPicked == true) {
     //   followAuto(mGenSpeeds(trajectory, ltvController));
     // }
-
+    SmartDashboard.putNumber("testing", drivetrainAuto.getState().Speeds.vxMetersPerSecond);
   }
 }
 
